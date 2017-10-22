c99a91d6964722d50611f87f08d8a05b056dba87
Gnat008 <maddock.evan@gmail.com>
2017-10-21 20:52:17 -0400

    start chat listener

---
+++ src/main/kotlin/me/ebonjaeger/chatenhancer/listeners/ChatListener.kt
@@ -0,0 +1,43 @@
+package me.ebonjaeger.chatenhancer.listeners
+
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.event.Listener
+import org.bukkit.event.player.AsyncPlayerChatEvent
+
+class ChatListener : Listener
+{
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    fun onPlayerChat(event: AsyncPlayerChatEvent)
+    {
+        val message = event.message
+        if (message.contains('@'))
+        {
+            val startIndex = message.indexOf('@') + 1
+            var endIndex = message.lastIndex
+            (startIndex..message.length)
+                    .filter { message[it] == ' ' }
+                    .forEach { endIndex = it }
+
+            val word = message.substring(startIndex, endIndex + 1)
+
+            var player: Player?
+            player = null
+            Bukkit.getServer().onlinePlayers
+                    .filter { it.name == word }
+                    .forEach { player = it }
+
+            if (player == null)
+            {
+                return
+            }
+
+            // Message contains an @-mention
+            event.recipients.remove(player)
+            player?.sendMessage("")
+        }
+    }
+}
