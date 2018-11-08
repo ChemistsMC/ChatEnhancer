3e8b2387445c8dd6f2e2e2e2fe8ec181a846bbf7
Evan Maddock <maddock.evan@vivaldi.net>
2018-11-08 14:08:13 -0500

    Mentions should only mention one player
    Also code reformat.
    
    Signed-off-by: Evan Maddock <maddock.evan@vivaldi.net>

--- src/main/kotlin/me/ebonjaeger/chatenhancer/listeners/ChatListener.kt
+++ src/main/kotlin/me/ebonjaeger/chatenhancer/listeners/ChatListener.kt
@@ -3,45 +3,33 @@ package me.ebonjaeger.chatenhancer.listeners
 import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 import org.bukkit.Sound
-import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.player.AsyncPlayerChatEvent
 
-class ChatListener : Listener
-{
+class ChatListener : Listener {
 
     @EventHandler(priority = EventPriority.MONITOR)
-    fun onPlayerChat(event: AsyncPlayerChatEvent)
-    {
+    fun onPlayerChat(event: AsyncPlayerChatEvent) {
         val message = event.message
-        if (message.contains('@'))
-        {
+
+        if (message.contains('@')) { // Message possibly contains a mention
             // Split the message into words, and add any word starting with
             // an '@' symbol to a new Set
-            val words = mutableSetOf<String>()
+            val words = mutableListOf<String>()
             message.split(' ')
-                    .filter { it.startsWith('@') }
-                    .forEach { words.add(it.substring(1).trim()) }
+                .filter { it.startsWith('@') }
+                .forEach { words.add(it.substring(1).trim()) }
 
-            // Iterate through all online players, and check if their name is mentioned.
-            // If mentioned, add them to a Set.
-            val players = mutableSetOf<Player>()
-            Bukkit.getServer().onlinePlayers.forEach { player ->
-                words.filter { player.name.equals(it, true) || player.name.startsWith(it, true) }
-                        .forEach { players.add(player) }
+            val players = Bukkit.matchPlayer(words[0])
+            if (players.size > 1) { // More than one player matches
+                return // Can't know which one was wanted, so do nothing
             }
 
-            if (players.isEmpty())
-            {
-                return
-            }
-
-            // Message contains an @-mention
+            // Message is mentioning a player
             event.recipients.removeAll(players)
-            for (player in players)
-            {
+            for (player in players) {
                 player.sendMessage(event.player.displayName + ": " + ChatColor.YELLOW + ChatColor.BOLD + message)
                 player.playSound(player.location, Sound.BLOCK_NOTE_BLOCK_CHIME, 1F, 1F)
             }
