c05d5db44c4403eb4b5498fda480c5791b8e0506
Evan Maddock <maddock.evan@vivaldi.net>
2018-11-08 14:09:28 -0500

    Code reformat
    Signed-off-by: Evan Maddock <maddock.evan@vivaldi.net>

--- src/main/kotlin/me/ebonjaeger/chatenhancer/ChatEnhancer.kt
+++ src/main/kotlin/me/ebonjaeger/chatenhancer/ChatEnhancer.kt
@@ -3,11 +3,9 @@ package me.ebonjaeger.chatenhancer
 import me.ebonjaeger.chatenhancer.listeners.ChatListener
 import org.bukkit.plugin.java.JavaPlugin
 
-class ChatEnhancer : JavaPlugin()
-{
+class ChatEnhancer : JavaPlugin() {
 
-    override fun onEnable()
-    {
+    override fun onEnable() {
         server.pluginManager.registerEvents(ChatListener(), this)
     }
 }
